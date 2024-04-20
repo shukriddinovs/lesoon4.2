21aae0ba5a4622624c35688ee932b93a8406ee60
shukriddinovs <sshukriddinov524@gmail.com>
2024-04-20 14:36:32 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,7 @@
+let numberONE = Number (prompt("enter a ="))
+let textONE = prompt ("eter your text =")
+let boolen1 = true;
+let boolen2 = false;
+let bigtest =656537444444444444444444444444444444494048459684986
+
+alert ("numberONE type = " + typeof(numberONE) +"textONE =" + typeof(textONE) + "boolen1 type =" +boolen1 + "/n boolen2 type =" +boolen2 );
